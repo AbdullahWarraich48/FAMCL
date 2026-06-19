c0c5df3790e1e30dab4a44164267375f8d73782d
AbdullahWarraich48 <abdullahwarraich0408@gmail.com>
2026-06-19 15:18:54 +0500

    changes

--- src/app/Accounts -and-Bookkeeping/page.tsx
+++ src/app/Accounts -and-Bookkeeping/page.tsx
@@ -10,7 +10,7 @@ export const metadata = {
     export default function AccountsAndBookkeepingPage() {
   return (
     <>
-      <BookKeepingHerosection />
+      {/* <BookKeepingHerosection /> */}
       <ServiceScope />
     </>
   );
